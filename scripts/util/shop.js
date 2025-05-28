27f8a29bcbaf5bbe9973bafe0b7febbd43b2e953
ginafender <123966430+birbothedev@users.noreply.github.com>
2025-05-28 14:46:23 -0400

    added notes and empty functions to shop

--- scripts/util/shop.js
+++ scripts/util/shop.js
@@ -20,3 +20,22 @@ function subtractCurrency(int){
     getPlayer().setCurrency(currentCurrency);
     updateCurrencyText();
 }
+
+function unlockDiagonalMove(){
+    // TODO unlock ability to move diagonally
+}
+
+function unlockGridVisibility(){
+    // TODO unlock ability to see full 3x3 grid around player
+}
+
+function unlockFreezeEnemy(){
+    // TODO unlock ability to freeze enemy on tile
+}
+
+function buyHealth(){
+    // TODO buy health to heal player in various amounts (20, 60, 100)
+}
+
+// TODO add function to control inventory so player can store items
+// TODO ^ rather than using them instantly upon purchase
